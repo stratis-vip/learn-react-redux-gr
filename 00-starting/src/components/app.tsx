7d60be86df6d044f78fa8f734e06fbf5c3bf0bfe
Stratis Christodoulou <stratis.vip@gmail.com>
2019-11-25 20:02:33 +0200

    Add files via upload

---
+++ 00-starting/src/components/app.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Props } from '../intefaces'
+
+const App = (props: Props) => <div>{props.text}</div>
+
+export default App
---
+++ 01-app-start/src/app.tsx
@@ -0,0 +1,8 @@
+/* ./src/app.tsx */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+
+ReactDOM.render(
+	<App text="Δοκιμαστικό Component" />, document.getElementById('root') //Component "App" στο div με id "root"
+)
